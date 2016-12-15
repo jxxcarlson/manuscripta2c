89a28ae6681c622ba1288d9e9e371d9a0128b288
James Carlson <jxxcarlson@gmail.com>
2016-12-14 19:21:24 -0500

    Parameterize [TextPageHeight] directive so as to make it work in different pages

--- src/app/text_helpers/textpageheight.directive.ts
+++ src/app/text_helpers/textpageheight.directive.ts
@@ -1,20 +1,22 @@
-import {Directive, ElementRef, Renderer, Input} from '@angular/core';
+import {Directive, ElementRef, Renderer, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[textPageHeight]',
 })
 export class TextPageHeightDirective {
-
-  private _defaultHeight = '1000px';
+  @Input('textPageHeight') inset: string = "160"
 
   constructor(public el: ElementRef, public renderer: Renderer) {
 
-    this.height = window.innerHeight.toString()
-    // renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'lightblue');
-    renderer.setElementStyle(el.nativeElement, 'height', (window.innerHeight - 160).toString() + 'px' || this._defaultHeight );
+  }
+
+  ngOnInit() {
+
+
+    this.renderer.setElementStyle(this.el.nativeElement, 'height', (window.innerHeight - parseInt(this.inset)).toString() + 'px' );
 
   }
 
-  @Input('textPageHeight') height: string;
+
 
 }

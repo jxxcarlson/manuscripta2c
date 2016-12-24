56d48d82e00b727c042433ad999839c8e9cde1b2
James Carlson <jxxcarlson@gmail.com>
2016-12-24 02:05:23 -0500

    Implement preprocessor in active document reducer to insert 'simplePageScroll' in anchor links

--- src/app/state-management/reducers/activeDocument.reducer.ts
+++ src/app/state-management/reducers/activeDocument.reducer.ts
@@ -8,14 +8,19 @@ import {Document} from '../interfaces/document.interface'
 import {initialDocument} from '../interfaces/document.interface'
 import {ActionReducer, Action} from '@ngrx/store';
 
+const identity = (text) => text
+const prepareSimplePageScroll = (text) => text.replace(/<a href="#/g, '<a simplePageScroll href="#')
+
+const process_document = (document) =>
+  Object.assign(document,  { rendered_text: prepareSimplePageScroll(document.rendered_text) })
 
 export const activeDocumentReducer: ActionReducer<Document> = (state: Document = initialDocument, action: Action) => {
 
   switch (action.type) {
     case ActionTypes.SELECT_DOCUMENT:
-      return action.payload;
+      return process_document(action.payload);
     case ActionTypes.UPDATE_DOCUMENT:
-      return Object.assign (state, action.payload)
+      return Object.assign (state, process_document(action.payload))
     default:
       return state;
   }

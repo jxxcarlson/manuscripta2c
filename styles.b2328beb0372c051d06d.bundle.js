3491f0b05a321cf87023557319a03941d29efa9e
James Carlson <jxxcarlson@gmail.com>
2016-12-24 07:38:01 -0500

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{1082:function(n,o,c){n.exports=c(475)},475:function(n,o){}},[1082]);
+webpackJsonp([1,2],{1084:function(n,o,c){n.exports=c(480)},480:function(n,o){}},[1084]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
